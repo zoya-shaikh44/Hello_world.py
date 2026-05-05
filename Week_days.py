0d683f54613bac38ebce5869e07b63dd5f84f942
zoya-shaikh44 <noshadshaikh025@gmail.com>
2026-05-05 06:04:43 +0530

    Week_days.py

---
+++ Week_days.py
@@ -0,0 +1,18 @@
+# week days
+day = "5th"
+if day == "1st":
+    print ("Monday")
+elif day == "2nd":
+    print ("Tuesday")
+elif day == "3rd":
+    print ("Wednesday")
+elif day == "4th":
+    print ("Thursday")
+elif day == "5th":
+    print ("Friday")
+elif day == "6th":
+    print ("Saturday")
+elif day == "7th":
+    print ("Sunday")
+else:
+    print ("end")
