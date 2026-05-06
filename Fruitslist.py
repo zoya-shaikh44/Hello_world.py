13badf6780820fa85fdf0ba0c1ad868ef009cca0
zoya-shaikh44 <noshadshaikh025@gmail.com>
2026-05-06 17:03:09 +0530

    Create Fruitslist.py

---
+++ Fruitslist.py
@@ -0,0 +1,7 @@
+#fruitlist
+fruits=["Apple","Mango","Orange","Banana","papaya","Grapes","Watermelon","Muskmelon","kiwi"]
+for fruit in fruits:
+    if fruit == "mango":
+        continue 
+    print(fruit)
+        
