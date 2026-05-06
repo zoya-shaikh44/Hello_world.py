f5254558bd971c1770d883b999ee0cac9d4c95c5
zoya-shaikh44 <noshadshaikh025@gmail.com>
2026-05-06 22:15:55 +0530

    Create Pass_fail_system.py

---
+++ Pass_fail_system.py
@@ -0,0 +1,7 @@
+#pass or fail
+marks = int (input ("Enter your marks :"))
+attendance = int (input ("Enter your attendance :"))
+if marks >= 80 and attendance >= 75:
+    print ("pass")
+else:
+    print("fail")
