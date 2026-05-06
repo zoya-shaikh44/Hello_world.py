b82f502b6e27c370a0a877e369c41b5d093f8473
zoya-shaikh44 <noshadshaikh025@gmail.com>
2026-05-06 20:49:57 +0530

    Create login_system.py

---
+++ login_system.py
@@ -0,0 +1,11 @@
+login system 
+username = "zoya"
+password = "2244"
+username = input ("Enter your username:")
+password = input ("Enter your password:")
+if username == "zoya" and password == "2244":
+    print ("login")
+else:
+    print ("login failed")
+    
+
