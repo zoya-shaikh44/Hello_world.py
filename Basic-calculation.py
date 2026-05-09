7ee69957c38a3c06fb8470841db674aec64ba326
zoya-shaikh44 <noshadshaikh025@gmail.com>
2026-05-09 12:18:41 +0530

    Create Basic-calculation.py

---
+++ Basic-calculation.py
@@ -0,0 +1,20 @@
+#calculator 
+a = 10
+b = 15
+def add (a,b):
+    print (a+b)
+def sub (a,b):
+    print (a-b)
+def multi (a,b):
+    print (a*b)
+def div (a,b):
+    print (a//b)
+   
+add (a,b)
+sub (a,b)
+multi (a,b)
+div (a,b)
+    
+
+
+    
