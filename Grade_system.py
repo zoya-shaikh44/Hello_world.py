ce89c86f0b4bf5dc32279480dde23d101f2107d6
zoya-shaikh44 <noshadshaikh025@gmail.com>
2026-05-11 15:36:25 +0530

    Create Grade_system.py

---
+++ Grade_system.py
@@ -0,0 +1,18 @@
+#student grade system 
+marks = int (input ("Enter your marks ="))
+if marks >= 90:
+    print ("grade :A+")
+elif marks >= 75:
+    print ("grade :A")
+elif marks < 75 and marks > 60:
+    print ("B")
+elif marks > 40:
+    print ("grade :C")
+else :
+    print ("fail")
+
+
+    
+
+
+    
