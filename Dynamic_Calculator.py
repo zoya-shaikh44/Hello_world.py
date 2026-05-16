278f575c7cb519c28c8726a6e889623bd364e3ca
zoya-shaikh44 <noshadshaikh025@gmail.com>
2026-05-16 06:16:59 +0530

    Create Dynamic_Calculator.py

---
+++ Dynamic_Calculator.py
@@ -0,0 +1,16 @@
+#Dynamic_Calculator
+a = int (input (' Enter digit: '))
+b = int (input ('Enter digit: '))
+def addition (a,b):
+	print (a+b)
+def subtraction (a,b):
+	print (a+b)
+def multiplication (a,b):
+	print (a*b)
+def division (a,b):
+	print (a//b)
+	
+addition (a,b)
+subtraction (a,b)
+multiplication (a,b)
+division (a,b)
