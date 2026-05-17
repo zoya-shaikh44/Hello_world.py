e775043e8d66e45aee946177391305cb0b8fad54
ZOYA_SHAIKH <noshadshaikh025@gmail.com>
2026-05-17 21:33:01 +0530

    FruitWorld.py

---
+++ FruitWorld.py
@@ -0,0 +1,19 @@
+#fruitslist
+fruits = [ ]
+
+f1 = input ('ENTER FRUIT NAME :')
+fruits.append(f1)
+f2 = input ('ENTER FRUIT NAME :')
+fruits.append(f2)
+f3 = input ('ENTER FRUIT NAME :')
+fruits.append(f3)
+f4 = input ('ENTER FRUIT NAME :')
+fruits.append(f4)
+f5 = input ('ENTER FRUIT NAME :')
+fruits.append(f5)
+f6 = input ('ENTER FRUIT NAME :')
+fruits.append(f6)
+f7 = input ('ENTER FRUIT NAME :')
+fruits.append(f7)
+
+print (fruits)
