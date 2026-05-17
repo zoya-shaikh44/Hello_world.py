d2df7bbc9097d33b23726014128f3f158f6fcfec
ZOYA_SHAIKH <noshadshaikh025@gmail.com>
2026-05-17 21:28:22 +0530

    resultSystem.py

---
+++ resultSystem.py
@@ -0,0 +1,21 @@
+#student_marks
+marks = [ ]
+
+m1 = int (input ('Enter your marks here :'))
+marks.append(m1)
+m2 = int (input ('Enter your marks here :'))
+marks.append(m2)
+m3 = int (input ('Enter your marks here :'))
+marks.append(m3)
+m4 = int ( input ('Enter your marks here :'))
+marks.append(m4)
+m5 = int( input ('Enter your marks here :'))
+marks.append(m5)
+m6 = int (input ('Enter your marks here :'))
+marks.append(m6)
+
+marks.sort()
+
+print (marks)
+
+
