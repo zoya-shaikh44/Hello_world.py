3d79a2368d41887578753bf41ecfa97ca79a8943
ZOYA_SHAIKH <noshadshaikh025@gmail.com>
2026-05-21 12:50:53 +0530

    Find-language.py

---
+++ Find-language.py
@@ -0,0 +1,24 @@
+#find language
+d = {}
+
+name = input ('Enter friends name :')
+language = input ('Enter language name  : ')
+
+d.update ({name :language})
+
+name = input ('Enter friends name :')
+language = input ('Enter language name  : ')
+
+d.update ({name :language})
+
+name = input ('Enter friends name :')
+language = input ('Enter language name  : ')
+
+d.update ({name :language})
+
+name = input ('Enter friends name :')
+language = input ('Enter language name  : ')
+
+d.update ({name :language})
+
+print (d)
