21c8660dd21ed9111f3e462ab2d0d500939f99f6
ZOYA_SHAIKH <noshadshaikh025@gmail.com>
2026-05-24 15:10:20 +0530

    Number_Average.py

---
+++ Number_Average.py
@@ -0,0 +1,15 @@
+#function definition
+def avg():
+	a=int(input('ENTER YOUR NUMBER :'))
+	b = int(input('ENTER YOUR NUMBER:'))
+	c = int(input('ENTER YOUR NUMBER :'))
+ 
+	average = (a+b+c)/3
+	print (average)
+	
+avg() #function call
+print('Thank you!')
+avg() 
+print('Thank you!')
+avg() 
+print('Thank you!')
