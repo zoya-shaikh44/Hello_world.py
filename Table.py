cd5213c690d698a1167a2f6e2700c8bf05d695d7
ZOYA_SHAIKH <noshadshaikh025@gmail.com>
2026-05-25 17:41:38 +0530

    Table.py

---
+++ Table.py
@@ -0,0 +1,28 @@
+n =int (input ('Enter table :'))
+for i in range (1,11):
+	print (f'{n} × {i} = {n*i}')
+	
+n1 = int (input ('Enter table :'))
+for i in range (1,11):
+	print (f'{n1} × {i} = {n1*i}')
+	
+n2 =int (input ('Enter table :'))
+for i in range (1,11):
+	print (f'{n2} × {i} = {n2*i}')
+	
+n3 = int (input ('Enter table:'))
+for i in range (1,11):
+	print (f'{n3} × {i} = {n3*i}')
+	
+n4 =int (input ('Enter table :'))
+for i in range (1,11):
+	print (f'{n4} × {i} = {n4*i}')
+	
+n5 = int (input ('Enter table :'))
+for i in range (1,11):
+	print (f'{n5} × {i} = {n5*i}')
+	
+n6 =int (input ('Enter table:'))
+for i in range (1,11):
+	print (f'{n6} × {i} = {n6*i}')
+
